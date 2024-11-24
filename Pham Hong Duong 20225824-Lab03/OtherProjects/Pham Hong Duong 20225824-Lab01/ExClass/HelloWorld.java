2493ad509a24e48f1681825cd9da5951ace22ce2
duongbhl <duongphamhong279@gmail.com>
2024-11-24 20:09:21 +0700

    u

--- Pham Hong Duong 20225824-Lab03/OtherProjects/Pham Hong Duong 20225824-Lab01/ExClass/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package TNOOP;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban\n");
-        System.out.println("Hello \t world!");
-    }
-}
